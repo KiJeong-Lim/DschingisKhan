9385b1682b1e6ce930933a19a8e869397c2fff8c
KiJeong-Lim <gijungdduk@naver.com>
2021-08-12 20:53:05 +0900

    ***

--- projects/PropositionalLogic/MetaTheorems.v
+++ projects/PropositionalLogic/MetaTheorems.v
@@ -1005,7 +1005,233 @@ Module CompletenessOfPL.
     preimage AtomF
   .
 
-  Parameter ModelExistsIfConsistent : forall hs : ensemble formula, ~ hs |- ContradictionF -> MaximalConsistentSet hs == eval_formula (makeEnv (MaximalConsistentSet hs)).
+  Theorem ModelExistsIfConsistent :
+    forall hs : ensemble formula,
+    ~ hs |- ContradictionF ->
+    MaximalConsistentSet hs == eval_formula (makeEnv (MaximalConsistentSet hs)).
+  Proof with eauto with *.
+    assert (lemma1 := @isSubsetOf_singleton formula).
+    assert (lemma2 : forall hs : ensemble formula, forall h : formula, isSubsetOf hs (insert h hs)).
+    { intros hs h b.
+      rewrite in_insert_iff...
+    }
+    assert (lemma3 := @isSubsetOf_empty formula).
+    assert (lemma4 : forall hs : ensemble formula, forall h : formula, member h (insert h hs)).
+    { intros hs h.
+      apply in_insert_iff...
+    }
+    intros hs claim1.
+    set (hs_hat := MaximalConsistentSet hs).
+    destruct (theorem_of_1_3_10 hs) as [claim2 [claim3 [claim4 [claim5 claim6]]]].
+    fold hs_hat in claim2, claim3, claim4, claim5, claim6.
+    assert (claim7 := TH_isSubsetOf_Cl).
+    assert (claim8 := Cl_isSubsetOf_TH).
+    assert (claim9 : equiconsistent (Cl hs) hs_hat).
+    { split.
+      - apply inconsistent_isSubsetOf.
+        transitivity (TH hs)...
+      - intros H.
+        apply (inconsistent_isSubsetOf (TH hs))...
+        apply claim3...
+    }
+    assert (claim10 : ~ inconsistent hs_hat).
+    { intros H.
+      contradiction claim1.
+      apply property1_of_inconsistent, claim9...
+    }
+    assert (claim11 : ~ inconsistent (Cl hs_hat)).
+    { intros H.
+      contradiction claim10.
+      apply (inconsistent_isSubsetOf (Cl (MaximalConsistentSet hs)))...
+      apply fact5_of_1_2_8, theorem_of_1_2_14, lemma1_of_1_3_8.
+    }
+    assert ( case_AtomF :
+      forall i : pvar,
+      member (AtomF i) hs_hat <-> member i (preimage AtomF hs_hat)
+    ).
+    { intros i.
+      rewrite in_preimage_iff...
+    }
+    assert ( case_ContradictonF :
+      member ContradictionF hs_hat <-> eval_formula (preimage AtomF hs_hat) ContradictionF
+    ).
+    { simpl.
+      rewrite claim4.
+      rewrite property1_of_inconsistent.
+      tauto.
+    }
+    assert ( case_NegationF :
+      forall p : formula,
+      (member p hs_hat <-> eval_formula (preimage AtomF hs_hat) p) ->
+      (member (NegationF p) hs_hat <-> eval_formula (preimage AtomF hs_hat) (NegationF p))
+    ).
+    { intros p IHp.
+      simpl.
+      rewrite <- IHp.
+      rewrite claim4.
+      split.
+      - intros H H0.
+        contradiction claim11.
+        apply property1_of_inconsistent.
+        apply (ContradictionI p).
+        + apply claim4...
+        + apply H.
+      - intros H.
+        apply claim4, claim5.
+        intros H0.
+        apply claim4.
+        apply (ContradictionI (NegationF p)).
+        + enough (aux1 : MaximalConsistentSet hs |- ImplicationF p ContradictionF).
+          { apply NegationI.
+            apply (ImplicationE p).
+            - apply (extendInfers (ImplicationF p ContradictionF) aux1)...
+            - apply ByAssumption...
+          }
+          apply claim4.
+          apply claim6.
+          intros H1.
+          contradiction H.
+        + apply ByAssumption...
+    }
+    assert ( case_ConjunctionF :
+      forall p1 : formula,
+      forall p2 : formula,
+      (member p1 hs_hat <-> eval_formula (preimage AtomF hs_hat) p1) ->
+      (member p2 hs_hat <-> eval_formula (preimage AtomF hs_hat) p2) ->
+      (member (ConjunctionF p1 p2) hs_hat <-> eval_formula (preimage AtomF hs_hat) (ConjunctionF p1 p2))
+    ).
+    { intros p1 p2 IHp1 IHp2.
+      simpl.
+      rewrite <- IHp1, <- IHp2.
+      split.
+      - intros H.
+        split.
+        + apply claim4.
+          apply (ConjunctionE1 p1 p2).
+          apply claim4...
+        + apply claim4.
+          apply (ConjunctionE2 p1 p2).
+          apply claim4...
+      - intros [H H0].
+        apply claim4.
+        apply ConjunctionI.
+        + apply claim4...
+        + apply claim4...
+    }
+    assert ( case_DisjunctionF :
+      forall p1 : formula,
+      forall p2 : formula,
+      (member p1 hs_hat <-> eval_formula (preimage AtomF hs_hat) p1) ->
+      (member p2 hs_hat <-> eval_formula (preimage AtomF hs_hat) p2) ->
+      (member (DisjunctionF p1 p2) hs_hat <-> eval_formula (preimage AtomF hs_hat) (DisjunctionF p1 p2))
+    ).
+    { intros p1 p2 IHp1 IHp2.
+      simpl.
+      rewrite <- IHp1, <- IHp2.
+      split.
+      - intros H.
+        destruct (classic (hs_hat |- p1)) as [H_yes | H_no].
+        + left.
+          apply claim4...
+        + right.
+          apply claim4.
+          apply (ImplicationE (NegationF p1)).
+          { apply (DisjunctionE p1 p2 (ImplicationF (NegationF p1) p2)).
+            - apply claim4...
+            - apply (ImplicationI (NegationF p1) p2).
+              apply (ContradictionE p2).
+              apply (ContradictionI p1).
+              + apply ByAssumption, in_insert_iff, or_intror...
+              + apply ByAssumption...
+            - apply ImplicationI.
+              apply ByAssumption, in_insert_iff, or_intror...
+          }
+          { apply claim4.
+            apply case_NegationF.
+            - apply IHp1.
+            - simpl.
+              intros H0.
+              assert (H1 := proj2 IHp1 H0).
+              apply claim4 in H1.
+              contradiction H_no.
+          }
+      - intros [H | H].
+        + apply claim4.
+          apply (DisjunctionI1 p1 p2).
+          apply claim4...
+        + apply claim4.
+          apply (DisjunctionI2 p1 p2).
+          apply claim4...
+    }
+    assert ( case_ImplicationF :
+      forall p1 : formula,
+      forall p2 : formula,
+      (member p1 hs_hat <-> eval_formula (preimage AtomF hs_hat) p1) ->
+      (member p2 hs_hat <-> eval_formula (preimage AtomF hs_hat) p2) ->
+      (member (ImplicationF p1 p2) hs_hat <-> eval_formula (preimage AtomF hs_hat) (ImplicationF p1 p2))
+    ).
+    { intros p1 p2 IHp1 IHp2.
+      unfold isImplicationFaithful in claim6.
+      rewrite claim6.
+      simpl.
+      tauto.
+    }
+    assert ( case_BiconditionalF :
+      forall p1 : formula,
+      forall p2 : formula,
+      (member p1 hs_hat <-> eval_formula (preimage AtomF hs_hat) p1) ->
+      (member p2 hs_hat <-> eval_formula (preimage AtomF hs_hat) p2) ->
+      (member (BiconditionalF p1 p2) hs_hat <-> eval_formula (preimage AtomF hs_hat) (BiconditionalF p1 p2))
+    ).
+    { intros p1 p2 IHp1 IHp2.
+      simpl.
+      transitivity (member (ImplicationF p1 p2) hs_hat /\ member (ImplicationF p2 p1) hs_hat).
+      { split.
+        - intros H.
+          split.
+          { apply claim4.
+            apply ImplicationI.
+            apply (BiconditionalE1 p1 p2).
+            - apply (@extendInfers hs_hat).
+              + apply claim4...
+              + apply lemma2.
+            - apply ByAssumption... 
+          }
+          { apply claim4.
+            apply ImplicationI.
+            apply (BiconditionalE2 p1 p2).
+            - apply (@extendInfers hs_hat).
+              + apply claim4...
+              + apply lemma2.
+            - apply ByAssumption... 
+          }
+        - intros [H H0].
+          apply claim4.
+          apply (BiconditionalI p1 p2).
+          { apply (ImplicationE p1 p2).
+            - apply (@extendInfers hs_hat).
+              + apply claim4...
+              + apply lemma2.
+            - apply ByAssumption...
+          }
+          { apply (ImplicationE p2 p1).
+            - apply (@extendInfers hs_hat).
+              + apply claim4...
+              + apply lemma2.
+            - apply ByAssumption...
+          }
+      }
+      { split.
+        - intros [H H0].
+          apply case_ImplicationF in H, H0...
+        - intros [H H0].
+          apply case_ImplicationF in H, H0...
+      }
+    }
+    intros p.
+    enough (it_is_sufficient_to_show : member p hs_hat <-> eval_formula (preimage AtomF hs_hat) p) by exact it_is_sufficient_to_show.
+    induction p...
+  Qed.
 
   Corollary Completeness :
     forall hs : ensemble formula,
