b780708ee1ae7fd3b34e251b89b58b0be4c2c74c
KiJeong-Lim <gijungdduk@naver.com>
2021-09-08 15:44:57 +0900

    .

--- pure/DomainTheory.v
+++ pure/DomainTheory.v
@@ -1045,11 +1045,11 @@ Module PowerSetCoLa.
       the left-lower path guarantees the existence of right-upper path
       on each following squares:
     % ===================== % ===================== % The left one means that:
-    % The square for (1)    % The square for (2)    % if the state of $Src$ moves from $s_1$ to $s_2$ along the road labelled $e$ and $R s_2 t_2$ holds
-    % ===================== % ===================== % then $R s_1 t_1$ holds and the state of $Tgt$ moves from $t_1$ to $t_2$ along the road labelled $e$.
+    % The square for (1)    % The square for (2)    % $R s_1 t_1$ holds and the state of $Tgt$ moves from $t_1$ to $t_2$ along the road labelled $e$
+    % ===================== % ===================== % if the state of $Src$ moves from $s_1$ to $s_2$ along the road labelled $e$ and $R s_2 t_2$ holds.
     %  s_1 ---- R ---> t_1  %  t_1 --- R^T --> s_1  % The right one means that:
-    %   |               |   %   |               |   % if the state of $Tgt$ moves from $t_1$ to $t_2$ along the road labelled $e$ and $R s_2 t_2$ holds
-    %   |               |   %   |               |   % then $R s_1 t_1$ holds and the state of $Src$ moves from $s_1$ to $s_2$ along the road labelled $e$.
+    %   |               |   %   |               |   % $R s_1 t_1$ holds and the state of $Src$ moves from $s_1$ to $s_2$ along the road labelled $e$
+    %   |               |   %   |               |   % if the state of $Tgt$ moves from $t_1$ to $t_2$ along the road labelled $e$ and $R s_2 t_2$ holds.
     % F_S e           F_T e % F_T e           F_S e %
     %   |               |   %   |               |   %
     %  \|/             \|/  %  \|/             \|/  %
