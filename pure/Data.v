7e16062c45a72e7eb2c930dfc5652cef68f5f107
KiJeong-Lim <gijungdduk@naver.com>
2022-01-27 11:41:15 +0900

    Data.v

---
+++ pure/Data.v
@@ -0,0 +1,13 @@
+Require Import Coq.Arith.PeanoNat.
+Require Import Coq.Bool.Bool.
+Require Import Coq.Classes.RelationClasses.
+Require Import Coq.Lists.List.
+Require Import Coq.micromega.Lia.
+Require Import DschingisKhan.pure.MyStructures.
+Require Import DschingisKhan.pure.MyUtilities.
+
+Module BinaryTrees.
+
+  Import ListNotations MyUtilities.
+
+End BinaryTrees.
