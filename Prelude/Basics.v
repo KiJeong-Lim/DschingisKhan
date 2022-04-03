70f87de4a1372ab76d52a8c47b17466422544ebf
KiJeong-Lim <gijungdduk@naver.com>
2022-04-04 03:40:11 +0900

    .

--- Prelude/Basics.v
+++ Prelude/Basics.v
@@ -6,7 +6,7 @@ Require Import Coq.Setoids.Setoid.
 
 Module BasicCategories.
 
-  Polymorphic Class isCategory (objs : Type) : Type :=
+  Polymorphic Class IsCategory (objs : Type) : Type :=
     { arrs (dom : objs) (cod : objs) : Type
     ; compose_arr {obj1 : objs} {obj2 : objs} {obj3 : objs} : arrs obj2 obj3 -> arrs obj1 obj2 -> arrs obj1 obj3
     ; id_arr {obj1 : objs} : arrs obj1 obj1
@@ -15,7 +15,7 @@ Module BasicCategories.
 
   Infix " \to " := arrs (at level 100, no associativity) : type_scope.
 
-  Polymorphic Definition Funktor {src_objs : Type} {tgt_objs : Type} (src_cat : isCategory src_objs) (tgt_cat : isCategory tgt_objs) : Type :=
+  Polymorphic Definition Funktor {src_objs : Type} {tgt_objs : Type} (src_cat : IsCategory src_objs) (tgt_cat : IsCategory tgt_objs) : Type :=
     src_objs -> tgt_objs
   .
 
@@ -23,23 +23,23 @@ Module BasicCategories.
 
   Section Defnitions_of_Functor_and_NaturalTransformation.
 
-  Polymorphic Context {src_objs : Type} {tgt_objs : Type} {src_cat : isCategory src_objs} {tgt_cat : isCategory tgt_objs}.
+  Polymorphic Context {src_objs : Type} {tgt_objs : Type} {src_cat : IsCategory src_objs} {tgt_cat : IsCategory tgt_objs}.
 
-  Polymorphic Class isFunctor (F : src_cat -----> tgt_cat) : Type :=
-    { fmap {dom : src_objs} {cod : src_objs} : (dom \to cod) -> (F dom \to F cod)
+  Polymorphic Class IsFunctor (f : src_cat -----> tgt_cat) : Type :=
+    { fmap {dom : src_objs} {cod : src_objs} : (dom \to cod) -> (f dom \to f cod)
     }
   .
 
-  Polymorphic Definition NatTrans (F_from : src_cat -----> tgt_cat) (F_to : src_cat -----> tgt_cat) : Type :=
-    forall obj : src_objs, F_from obj \to F_to obj
+  Polymorphic Definition NatTrans (f_from : src_cat -----> tgt_cat) (f_to : src_cat -----> tgt_cat) : Type :=
+    forall obj : src_objs, f_from obj \to f_to obj
   .
 
   End Defnitions_of_Functor_and_NaturalTransformation.
 
   Infix " =====> " := NatTrans (at level 100, no associativity) : type_scope.
 
-  Global Polymorphic Instance Hask : isCategory Type :=
-    { arrs (from_obj : Type) (to_obj : Type) := from_obj -> to_obj
+  Global Polymorphic Instance Hask : IsCategory Type :=
+    { arrs (dom : Type) (cod : Type) := dom -> cod
     ; compose_arr {A : Type} {B : Type} {C : Type} := compose (A := A) (B := B) (C := C)
     ; id_arr {A : Type} := id (A := A)
     }
