761758e300ae723692972065af74eddc4c378fb0
KiJeong-Lim <gijungdduk@naver.com>
2022-04-04 04:04:52 +0900

    .

--- Prelude/Basics.v
+++ Prelude/Basics.v
@@ -7,31 +7,29 @@ Require Import Coq.Setoids.Setoid.
 Module BasicCategories.
 
   Polymorphic Class IsCategory (objs : Type) : Type :=
-    { arrs (dom : objs) (cod : objs) : Type
-    ; compose_arr {obj1 : objs} {obj2 : objs} {obj3 : objs} : arrs obj2 obj3 -> arrs obj1 obj2 -> arrs obj1 obj3
-    ; id_arr {obj1 : objs} : arrs obj1 obj1
+    { hom (dom : objs) (cod : objs) : Type
+    ; compose_arr {obj_l : objs} {obj : objs} {obj_r : objs} : hom obj obj_r -> hom obj_l obj -> hom obj_l obj_r
+    ; id_arr {obj : objs} : hom obj obj
     }
   .
 
-  Infix " \to " := arrs (at level 100, no associativity) : type_scope.
-
-  Polymorphic Definition Funktor {src_objs : Type} {tgt_objs : Type} (src_cat : IsCategory src_objs) (tgt_cat : IsCategory tgt_objs) : Type :=
+  Polymorphic Definition ObjectMaps {src_objs : Type} {tgt_objs : Type} (src_cat : IsCategory src_objs) (tgt_cat : IsCategory tgt_objs) : Type :=
     src_objs -> tgt_objs
   .
 
-  Infix " -----> " := Funktor (at level 100, no associativity) : type_scope.
+  Infix " -----> " := ObjectMaps (at level 100, no associativity) : type_scope.
 
   Section Defnitions_of_Functor_and_NaturalTransformation.
 
   Polymorphic Context {src_objs : Type} {tgt_objs : Type} {src_cat : IsCategory src_objs} {tgt_cat : IsCategory tgt_objs}.
 
   Polymorphic Class IsFunctor (f : src_cat -----> tgt_cat) : Type :=
-    { fmap {dom : src_objs} {cod : src_objs} : (dom \to cod) -> (f dom \to f cod)
+    { fmap {dom : src_objs} {cod : src_objs} : hom dom cod -> hom (f dom) (f cod)
     }
   .
 
   Polymorphic Definition NatTrans (f_from : src_cat -----> tgt_cat) (f_to : src_cat -----> tgt_cat) : Type :=
-    forall obj : src_objs, f_from obj \to f_to obj
+    forall obj : src_objs, hom (f_from obj) (f_to obj)
   .
 
   End Defnitions_of_Functor_and_NaturalTransformation.
@@ -39,7 +37,7 @@ Module BasicCategories.
   Infix " =====> " := NatTrans (at level 100, no associativity) : type_scope.
 
   Global Polymorphic Instance Hask : IsCategory Type :=
-    { arrs (dom : Type) (cod : Type) := dom -> cod
+    { hom (dom : Type) (cod : Type) := dom -> cod
     ; compose_arr {A : Type} {B : Type} {C : Type} := compose (A := A) (B := B) (C := C)
     ; id_arr {A : Type} := id (A := A)
     }
