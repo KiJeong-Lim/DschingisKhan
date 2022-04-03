a34e2c22e641c7dde2d26c13d9dbb7874c093974
KiJeong-Lim <gijungdduk@naver.com>
2022-04-04 08:06:12 +0900

    .

--- Prelude/Basics.v
+++ Prelude/Basics.v
@@ -4,48 +4,63 @@ Require Import Coq.Relations.Relation_Definitions.
 Require Import Coq.Relations.Relation_Operators.
 Require Import Coq.Setoids.Setoid.
 
+Module MyInit.
+
+  Reserved Infix " -----> " (at level 100, no associativity).
+
+  Reserved Infix " =====> " (at level 100, no associativity).
+
+End MyInit.
+
 Module BasicCategories.
 
-  Polymorphic Class IsCategory (objs : Type) : Type :=
+  Import MyInit.
+
+  Polymorphic Class Category (objs : Type) : Type :=
     { hom (dom : objs) (cod : objs) : Type
     ; compose {obj_l : objs} {obj : objs} {obj_r : objs} : hom obj obj_r -> hom obj_l obj -> hom obj_l obj_r
     ; id {obj : objs} : hom obj obj
     }
   .
 
-  Polymorphic Definition ObjMap {src_objs : Type} {tgt_objs : Type} (src_cat : IsCategory src_objs) (tgt_cat : IsCategory tgt_objs) : Type := src_objs -> tgt_objs.
+  Polymorphic Definition ObjMap {src_objs : Type} {tgt_objs : Type} (src_cat : Category src_objs) (tgt_cat : Category tgt_objs) : Type := src_objs -> tgt_objs.
 
-  Infix " -----> " := ObjMap (at level 100, no associativity) : type_scope.
+  Infix " -----> " := ObjMap : type_scope.
 
-  Section Defnitions_of_Functor_and_NaturalTransformation.
+  Section BasicConceptsOfCategory.
 
-  Polymorphic Context {src_objs : Type} {tgt_objs : Type} {src_cat : IsCategory src_objs} {tgt_cat : IsCategory tgt_objs}.
+  Polymorphic Context {src_objs : Type} {tgt_objs : Type} {src_cat : Category src_objs} {tgt_cat : Category tgt_objs}.
 
-  Polymorphic Class IsFunctor (f : src_cat -----> tgt_cat) : Type :=
+  Polymorphic Class CovariantFunctor (f : src_cat -----> tgt_cat) : Type :=
     { fmap {dom : src_objs} {cod : src_objs} : hom dom cod -> hom (f dom) (f cod)
     }
   .
 
-  Polymorphic Definition NatTrans (f_from : src_cat -----> tgt_cat) (f_to : src_cat -----> tgt_cat) : Type := forall obj : src_objs, hom (f_from obj) (f_to obj).
+  Polymorphic Class ContravariantFunctor (f : src_cat -----> tgt_cat) : Type :=
+    { contramap {dom : src_objs} {cod : src_objs} : hom dom cod -> hom (f cod) (f dom)
+    }
+  .
 
-  End Defnitions_of_Functor_and_NaturalTransformation.
+  Polymorphic Definition NaturalTransformation (f_from : src_cat -----> tgt_cat) (f_to : src_cat -----> tgt_cat) : Type := forall obj : src_objs, hom (f_from obj) (f_to obj).
 
-  Infix " =====> " := NatTrans (at level 100, no associativity) : type_scope.
+  End BasicConceptsOfCategory.
+
+  Infix " =====> " := NaturalTransformation : type_scope.
 
 End BasicCategories.
 
 Module MyUniverses.
 
-  Universe LargeUniv_lv.
+  Universe Univ_lv.
 
-  Definition LargeUniv : Type@{LargeUniv_lv+1} := Type@{LargeUniv_lv}.
+  Definition Univ : Type@{Univ_lv+1} := Type@{Univ_lv}.
 
-  Polymorphic Definition Univ : LargeUniv := Type.
+  Polymorphic Definition hask : Univ := Type.
 
-  Global Polymorphic Instance Hask : BasicCategories.IsCategory Univ :=
-    { hom (dom : Univ) (cod : Univ) := dom -> cod
-    ; compose {A : Univ} {B : Univ} {C : Univ} := compose (A := A) (B := B) (C := C)
-    ; id {A : Univ} := id (A := A)
+  Global Polymorphic Instance Hask : BasicCategories.Category hask :=
+    { hom (dom : hask) (cod : hask) := dom -> cod
+    ; compose {A : hask} {B : hask} {C : hask} := compose (A := A) (B := B) (C := C)
+    ; id {A : hask} := id (A := A)
     }
   .
 
@@ -53,8 +68,8 @@ End MyUniverses.
 
 Module BasicEnsembles.
 
-  Import MyUniverses.
+  Import MyInit BasicCategories MyUniverses.
 
-  Polymorphic Definition ensemble (X : Univ) : Univ := X -> Prop.
+  Polymorphic Definition ensemble (X : hask) : hask := X -> Prop.
 
 End BasicEnsembles.
