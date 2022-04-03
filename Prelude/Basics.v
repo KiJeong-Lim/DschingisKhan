ba66917a4792727e6aee370d124d0cb8663322d9
KiJeong-Lim <gijungdduk@naver.com>
2022-04-04 03:01:20 +0900

    .

--- Prelude/Basics.v
+++ Prelude/Basics.v
@@ -7,22 +7,26 @@ Require Import Coq.Setoids.Setoid.
 Module BasicCategories.
 
   Polymorphic Class isCategory (objs : Type) : Type :=
-    { arrs (from_obj : objs) (to_obj : objs) : Type
+    { arrs (dom : objs) (cod : objs) : Type
     ; compose_arr {obj1 : objs} {obj2 : objs} {obj3 : objs} : arrs obj2 obj3 -> arrs obj1 obj2 -> arrs obj1 obj3
     ; id_arr {obj1 : objs} : arrs obj1 obj1
     }
   .
 
+  Infix " \to " := arrs (at level 100, no associativity) : type_scope.
+
   Polymorphic Class isFunctor {src_objs : Type} {tgt_objs : Type} {src_cat : isCategory src_objs} {tgt_cat : isCategory tgt_objs} (map_objs : src_objs -> tgt_objs) : Type :=
-    { map_arrs {from_obj : src_objs} {to_obj : src_objs} : arrs (objs := src_objs) from_obj to_obj -> arrs (objs := tgt_objs) (map_objs from_obj) (map_objs to_obj)
+    { map_arrs {dom : src_objs} {cod : src_objs} : (dom \to cod) -> (map_objs dom \to map_objs cod)
     }
   .
 
-  Polymorphic Definition isNaturalTransformation {src_objs : Type} {tgt_objs : Type} {src_cat : isCategory src_objs} {tgt_cat : isCategory tgt_objs} (from_map_objs : src_objs -> tgt_objs) (to_map_objs : src_objs -> tgt_objs) : Type :=
-    forall s_obj : src_objs, arrs (objs := tgt_objs) (from_map_objs s_obj) (to_map_objs s_obj)
+  Infix " -----> " := isFunctor (at level 100, no associativity) : type_scope.
+
+  Polymorphic Class isNaturalTransformation {src_objs : Type} {tgt_objs : Type} {src_cat : isCategory src_objs} {tgt_cat : isCategory tgt_objs} (_from : src_objs -> tgt_objs) (_to : src_objs -> tgt_objs) : Type :=
+    { component (obj : src_objs) : _from obj \to _to obj
+    }
   .
 
-  Infix " \to " := arrs (at level 100, no associativity) : type_scope.
   Infix " =====> " := isNaturalTransformation (at level 100, no associativity) : type_scope.
 
   Global Polymorphic Instance Hask : isCategory Type :=
