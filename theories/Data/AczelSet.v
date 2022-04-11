18f4c048c67b54715251d36d5b0efbfdb8ff4136
KiJeong-Lim <gijungdduk@naver.com>
2022-04-11 20:00:23 +0900

    .

--- theories/Data/AczelSet.v
+++ theories/Data/AczelSet.v
@@ -2,8 +2,10 @@ Require Import Coq.Classes.RelationClasses.
 Require Import Coq.Lists.List.
 Require Import Coq.Program.Basics.
 Require Import Coq.Relations.Relation_Definitions.
+Require Import Coq.Relations.Relation_Operators.
 Require Import Coq.Setoids.Setoid.
 Require Import DschingisKhan.Prelude.PreludeInit.
+Require Import DschingisKhan.Prelude.PreludeUtil.
 
 Module AczelSet.
 
@@ -15,24 +17,157 @@ Module AczelSet.
   | Node (children : Type@{AczelSet_lv}) (childtrees : children -> Tree) : Tree
   .
 
-  Polymorphic Definition getChildren@{lv} (root : Tree@{lv}) : Type@{lv} :=
-    match root return Type@{lv} with
+  Polymorphic Definition AczelSet@{lv} : Univ := Tree@{lv}.
+
+  Polymorphic Definition getChildren@{lv} (root : AczelSet@{lv}) : Type@{lv} :=
+    match root in Tree return Type@{lv} with
     | Node children childtrees => children
     end
   .
 
-  Polymorphic Definition getChildTrees@{lv} (root : Tree@{lv}) : getChildren@{lv} root -> Tree@{lv} :=
-    match root as t return getChildren@{lv} t -> Tree@{lv} with
+  Polymorphic Definition getChildTrees@{lv} (root : AczelSet@{lv}) : getChildren@{lv} root -> AczelSet@{lv} :=
+    match root as tree in Tree return getChildren@{lv} tree -> Tree@{lv} with
     | Node children childtrees => childtrees
     end
   .
 
-  Polymorphic Definition ordinalFromAcc@{lv} {A : Type@{lv}} {wfRel : A -> A -> Prop} : forall root : A, Acc wfRel root -> Tree@{lv} :=
-    fix ordinalFromAcc_fix (tree : A) (tree_acc : Acc wfRel tree) {struct tree_acc} : Tree :=
-    let children : Type@{lv} := {subtree : A | wfRel subtree tree} in
-    match tree_acc with
-    | Acc_intro _ hyp_acc => Node children (fun childtree : children => ordinalFromAcc_fix (proj1_sig childtree) (hyp_acc (proj1_sig childtree) (proj2_sig childtree)))
+(** "Equality and Membership" *)
+
+  Fixpoint equality (lhs : AczelSet) (rhs : AczelSet) : Prop :=
+    match lhs in Tree, rhs in Tree return Prop with
+    | Node children1 childtrees1, Node children2 childtrees2 => ⟪ lhs_subset_rhs : forall child1 : children1, exists child2 : children2, equality (childtrees1 child1) (childtrees2 child2) ⟫ /\ ⟪ rhs_subset_lhs : forall child2 : children2, exists child1 : children1, equality (childtrees1 child1) (childtrees2 child2) ⟫
     end
   .
+
+  Global Instance equality_Equivalence
+    : Equivalence equality.
+  Proof with eauto.
+    split.
+    - intros x; induction x as [x_children x_childtree IH].
+      cbn in *; split; unnw...
+    - intros x; induction x as [x_children x_childtree IH]; intros [y_children y_childtree] [x_subset_y y_subset_x].
+      cbn in *; split; unnw; [intros c_y | intros c_x].
+      + exploit (y_subset_x c_y) as [c_x hyp_eq1]...
+      + exploit (x_subset_y c_x) as [c_y hyp_eq1]...
+    - intros x y; revert x; induction y as [y_children y_childtree IH]; intros [x_children x_childtree] [z_children z_childtree] [x_subset_y y_subset_x] [y_subset_z z_subset_y].
+      cbn in *; split; unnw; [intros c_x | intros c_z].
+      + exploit (x_subset_y c_x) as [c_y hyp_eq1]; exploit (y_subset_z c_y) as [c_z hyp_eq2]...
+      + exploit (z_subset_y c_z) as [c_y hyp_eq1]; exploit (y_subset_x c_y) as [c_x hyp_eq2]...
+  Qed.
+
+  Global Instance AczelSet_isSetoid : isSetoid AczelSet :=
+    { eqProp := equality
+    ; eqProp_Equivalence := equality_Equivalence
+    }
+  .
+
+  Lemma unfold_eqProp_inAczelSet (lhs : AczelSet) (rhs : AczelSet)
+    : (lhs == rhs) = equality lhs rhs.
+  Proof. reflexivity. Qed.
+
+  Ltac fold_equality := rewrite <- unfold_eqProp_inAczelSet in *.
+
+  Definition elem (finger : AczelSet) (hand : AczelSet) : Prop :=
+    exists bone : getChildren hand, finger == getChildTrees hand bone
+  .
+
+  Local Infix " `elem` " := elem (at level 70, no associativity) : type_scope.
+
+  Lemma unfold_elem (finger : AczelSet) (hand : AczelSet)
+    : finger `elem` hand = (exists bone : getChildren hand, finger == getChildTrees hand bone).
+  Proof. reflexivity. Qed.
+
+  Lemma elem_intro {z : AczelSet} {x : AczelSet} (c_x : getChildren x)
+    (z_eq_x_c : z == getChildTrees x c_x)
+    : z `elem` x.
+  Proof.
+    exists c_x. exact (z_eq_x_c).
+  Qed.
+
+  Lemma eqProp_elem_elem (x : AczelSet) (y : AczelSet) (z : AczelSet)
+    (x_eq_y : x == y)
+    (y_in_z : y `elem` z)
+    : x `elem` z.
+  Proof.
+    destruct y_in_z as [c_z y_eq_z_c]. eexists.
+    etransitivity; [exact (x_eq_y) | exact (y_eq_z_c)].
+  Qed.
+
+  Lemma elem_eqProp_elem (x : AczelSet) (y : AczelSet) (z : AczelSet)
+    (y_eq_z : y == z)
+    (x_in_y : x `elem` y)
+    : x `elem` z.
+  Proof.
+    destruct z as [z_children z_childtree]; destruct y as [y_children y_childtree].
+    destruct y_eq_z as [y_subset_z_c z_c_subset_y]; destruct x_in_y as [c_y x_eq_y_c].
+    destruct (y_subset_z_c c_y) as [c_z c_y_eq_c_z]. cbn in *.
+    eexists. etransitivity; [exact (x_eq_y_c) | exact (c_y_eq_c_z)].
+  Qed.
+
+  Global Add Parametric Morphism :
+    elem with signature (eqProp ==> eqProp ==> iff)
+  as elem_compatWith_eqProp.
+  Proof with eauto.
+    intros x1 y1 hyp_eq1 x2 y2 hyp_eq2.
+    transitivity (x1 `elem` y2); split.
+    - eapply elem_eqProp_elem...
+    - eapply elem_eqProp_elem. symmetry...
+    - eapply eqProp_elem_elem. symmetry...
+    - eapply eqProp_elem_elem...
+  Qed.
+
+  Local Add Parametric Morphism :
+    (Acc elem) with signature (eqProp ==> iff)
+  as Acc_elem_compatWith_eqProp.
+  Proof.
+    enough (to_show : forall lhs : AczelSet, forall rhs : AczelSet, lhs == rhs -> Acc elem lhs -> Acc elem rhs).
+    { intros x y h_eq. split; eapply to_show; [exact (h_eq) | symmetry; exact (h_eq)]. }
+    intros lhs rhs x_eq_y acc_x. cbn. pose proof (Acc_inv acc_x) as claim1. econstructor.
+    intros z z_in_y. eapply claim1. rewrite x_eq_y. exact (z_in_y). 
+  Qed.
+
+  Lemma AczelSet_elem_well_founded
+    : forall root : AczelSet, Acc elem root.
+  Proof.
+    induction root as [x_children x_childtree IH]. econstructor.
+    intros z [c_x z_eq_x_c]. rewrite z_eq_x_c. exact (IH c_x).
+  Qed.
+
+  Global Instance AczelSet_isWellFounded : isWellFounded AczelSet :=
+    { wfRel := elem
+    ; wfRel_well_founded := AczelSet_elem_well_founded
+    }
+  .
+
+  Definition fromAcc {A : Type} {wfRel : A -> A -> Prop} : forall root : A, Acc wfRel root -> Tree :=
+    fix fromAcc_fix (tree : A) (tree_acc : Acc wfRel tree) {struct tree_acc} : Tree :=
+    let children : Type := {subtree : A | wfRel subtree tree} in
+    match tree_acc with
+    | Acc_intro _ hyp_acc => Node children (fun childtree : children => fromAcc_fix (proj1_sig childtree) (hyp_acc (proj1_sig childtree) (proj2_sig childtree)))
+    end
+  .
+
+(** "Ordinals" *)
+
+  Definition isTransitiveSet (x : AczelSet) : Prop :=
+    forall y : AczelSet, y `elem` x ->
+    forall z : AczelSet, z `elem` y ->
+    z `elem` x
+  .
+
+  Variant isOrdinal (alpha : AczelSet) : Prop :=
+  | transitive_set_of_transtive_sets_isOrdinal
+    (alpha_isTranstiveSet : isTransitiveSet alpha)
+    (every_member_of_alpha_isTranstiveSet : forall beta, beta `elem` alpha -> isTransitiveSet beta)
+    : isOrdinal alpha
+  .
+
+  Lemma every_member_of_Ordinal_isOrdinal (alpha : AczelSet)
+    (alpha_isOrdinal : isOrdinal alpha)
+    : forall beta : AczelSet, beta `elem` alpha -> isOrdinal beta.
+  Proof.
+    inversion alpha_isOrdinal; subst.
+    intros beta beta_in_alpha; econstructor; eauto.
+  Qed.
 
 End AczelSet.
