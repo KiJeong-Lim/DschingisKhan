35678aa4fc1bacda8f0109e9c4d6e023ca2328e9
KiJeong-Lim <gijungdduk@naver.com>
2022-04-19 23:48:16 +0900

    .

--- theories/Data/InteractionTree.v
+++ theories/Data/InteractionTree.v
@@ -6,6 +6,8 @@ Require Import DschingisKhan.Prelude.PreludeInit.
 
 Module InteractionTrees.
 
+  Global Notation " E -< F " := (forall X : Type, E X -> F X) (at level 100, no associativity) : type_scope.
+
   Variant itreeF (itree_E_R : Type) (E : Type -> Type) (R : Type) : Type :=
   | RetF (r : R) : itreeF itree_E_R E R
   | TauF (t : itree_E_R) : itreeF itree_E_R E R
@@ -83,7 +85,7 @@ Module InteractionTrees.
 
   Definition itree_tick {E : Type -> Type} : itree E unit := Tau (Ret tt).
 
-  Definition itree_trigger {E : Type -> Type} (R : Type) (e : E R) : itree E R := Vis R e (fun x : R => Ret x).
+  Definition itree_trigger {E : Type -> Type} : E -< itree E := fun R : Type => fun e : E R => Vis R e (fun x : R => Ret x).
 
   Definition itree_iter {E : Type -> Type} {I : Type} {R : Type} (step : I -> itree E (I + R)%type) : I -> itree E R :=
     cofix itree_iter_cofix (i : I) : itree E R :=
@@ -106,5 +108,73 @@ Module InteractionTrees.
       end
     )
   .
+
+  Definition itree_interpret_stateT {E : Type -> Type} {E' : Type -> Type} {ST : Type} (handle : E =====> stateT ST (itree E')) : itree E =====> stateT ST (itree E') :=
+    itree_interpret (E := E) (M := stateT ST (itree E')) (M_isMonadIter := stateT_ST_isMonadIter ST (itree E')) handle
+  .
+
+  Inductive callE (I : Type) (R : Type) : Type -> Type :=
+  | Call (arg : I) : callE I R R
+  .
+
+  Global Arguments Call {I} {R}.
+
+  Section RECURSION. (* Reference: "https://github.com/DeepSpec/InteractionTrees/blob/5fe86a6bb72f85b5fcb125da10012d795226cf3a/theories/Interp/Recursion.v" *)
+
+  Definition itree_interpret_mrec {E1 : Type -> Type} {E2 : Type -> Type} (ctx : E1 =====> itree (E1 +' E2)) : itree (E1 +' E2) =====> itree E2 :=
+    fun R : Type =>
+    itree_iter (E := E2) (I := itree (E1 +' E2) R) (R := R) (
+      fun t0 : itree (E1 +' E2) R =>
+      match observe t0 with
+      | RetF r => Ret (inr r)
+      | TauF t => Ret (inl t)
+      | VisF X e k =>
+        match e with
+        | inl1 e1 => Ret (inl (bind (ctx X e1) k))
+        | inr1 e2 => Vis X e2 (fun x : X => pure (inl (k x)))
+        end
+      end
+    )
+  .
+
+  Definition itree_mrec {E : Type -> Type} {E' : Type -> Type} (ctx : E =====> itree (E +' E')) : E =====> itree E' :=
+    fun R : Type =>
+    fun e : E R =>
+    itree_interpret_mrec (E1 := E) (E2 := E') ctx R (ctx R e)
+  .
+
+  Definition itree_trigger_inl1 {E : Type -> Type} {E' : Type -> Type} : E -< itree (E +' E') :=
+    fun R : Type =>
+    fun e : E R =>
+    itree_trigger (E := E +' E') R (inl1 e)
+  .
+
+  Local Notation endo X := (X -> X).
+
+  Definition itree_mrec_fix {E : Type -> Type} {E' : Type -> Type} (ctx : endo (E -< itree (E +' E'))) : E -< itree E' :=
+    itree_mrec (E := E) (E' := E') (ctx itree_trigger_inl1)
+  .
+
+  Definition itree_app {E : Type -> Type} {I : Type} {R : Type} (callee : I -> itree E R) : callE I R =====> itree E :=
+    fun X : Type =>
+    fun e : callE I R X =>
+    match e in callE _ _ r return itree E r with
+    | Call arg => callee arg
+    end
+  .
+
+  Definition itree_rec {E : Type -> Type} {I : Type} {R : Type} (body : I -> itree (callE I R +' E) R) (arg : I) : itree E R :=
+    itree_mrec (E := callE I R) (E' := E) (itree_app body) R (Call arg)
+  .
+
+  Definition itree_call {E : Type -> Type} {I : Type} {R : Type} (arg : I) : itree (callE I R +' E) R :=
+    itree_trigger_inl1 (E := callE I R) (E' := E) R (Call arg)
+  .
+
+  Definition itree_rec_fix {E : Type -> Type} {I : Type} {R : Type} (body : endo (I -> itree (callE I R +' E) R)) : I -> itree E R :=
+    itree_rec (E := E) (I := I) (R := R) (body itree_call)
+  .
+
+  End RECURSION.
 
 End InteractionTrees.
