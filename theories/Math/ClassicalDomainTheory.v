3851c3d4ed69a20c1b7c163df68635863b3c9f79
KiJeong-Lim <gijungdduk@naver.com>
2022-06-17 11:46:36 +0900

    .

--- theories/Math/ClassicalDomainTheory.v
+++ theories/Math/ClassicalDomainTheory.v
@@ -27,15 +27,12 @@ Module BasicCpoTheory.
     : isOpen (U x).
   Proof. (* Thanks to Junyoung Jang *)
     split.
-    - intros y z y_in_U_x y_le_z z_le_x. unnw.
-      contradiction y_in_U_x. now transitivity (z).
+    - intros y z y_in_U_x y_le_z z_le_x. unnw. contradiction y_in_U_x. now transitivity (z).
     - intros X X_nonempty X_isDirected sup_X sup_X_isSupremumOf_X sup_X_in_U_x. unnw.
       assert (JunyoungJang'sAdvice : ~ << UPPER_BOUND : forall z : D, member z X -> z =< x >>).
-      { intros UPPER_BOUND. contradiction (sup_X_in_U_x). now eapply sup_X_isSupremumOf_X. }
-      eapply ExclusiveMiddle.NNPP. intros H_false.
-      contradiction JunyoungJang'sAdvice. intros y y_in_X.
-      eapply ExclusiveMiddle.NNPP. intros y_in_U_x.
-      contradiction H_false. now exists (y).
+      { intros UPPER_BOUND. contradiction sup_X_in_U_x. now eapply sup_X_isSupremumOf_X. }
+      eapply ExclusiveMiddle.NNPP. intros H_false. contradiction JunyoungJang'sAdvice. intros y y_in_X.
+      eapply ExclusiveMiddle.NNPP. intros y_in_U_x. contradiction H_false. now exists (y).
   Qed.
 
   Lemma ContinuousMap_isMonotonicMap {dom : Type} {cod : Type} {dom_isPoset : isPoset dom} {cod_isPoset : isPoset cod} (f : dom -> cod)
@@ -44,7 +41,7 @@ Module BasicCpoTheory.
   Proof.
     intros x1 x2 x1_le_x2. eapply ExclusiveMiddle.NNPP. intros f_x1_in_U_f_x2.
     assert (x1_in_preimage_f_U_f_x2 : member x1 (preimage f (U (f x2)))) by now econstructor.
-    assert (preimage_f_U_f_x2_isOpen : isOpen (preimage f (U (f x2)))) by now eapply f_isContinuousMap, U_x_isOpen.
+    assert (preimage_f_U_f_x2_isOpen : isOpen (preimage f (U (f x2)))) by eapply f_isContinuousMap, U_x_isOpen.
     assert (x2_in_preimage_f_U_f_x2 : member x2 (preimage f (U (f x2)))).
     { inversion preimage_f_U_f_x2_isOpen. eapply UPWARD_CLOSED with (x := x1); eauto. }
     assert (f_x2_in_U_f_x2 : member (f x2) (U (f x2))) by now inversion x2_in_preimage_f_U_f_x2; subst.
