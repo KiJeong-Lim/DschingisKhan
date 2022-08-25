341f30b462433e0f7458db1f6434f1981734a5e5
KiJeong-Lim <gijungdduk@naver.com>
2022-08-25 20:54:31 +0900

    .

--- theories/Math/GIT.v
+++ theories/Math/GIT.v
@@ -22,18 +22,20 @@ Module GIT.
 
   Local Existing Instances freeSetoidFromSetoid1.
 
-  Definition isPartialFunctionGraph {arity : nat} (funGraph : ensemble (vector nat arity * nat)) : Prop :=
-    forall x : vector nat arity, forall y : nat, forall y' : nat, (x, y) \in funGraph -> (x, y') \in funGraph -> y = y'
+  Definition isPartialFunctionGraph {arity : nat} (fun_graph : ensemble (vector nat arity * nat)) : Prop :=
+    forall x : vector nat arity, forall y : nat, forall y' : nat, (x, y) \in fun_graph -> (x, y') \in fun_graph -> y = y'
   .
 
   Definition converges_to {arity : nat} (computer : vector nat arity -> itree void1 nat) (arg : vector nat arity) (rv : nat) : Prop :=
     exists fuel : nat, burnTau_nat fuel (computer arg) == Ret rv
   .
 
-  Record muPartial {arity : nat} (funGraph : ensemble (vector nat arity * nat)) : Type :=
+  Record muPartial {arity : nat} (fun_graph : ensemble (vector nat arity * nat)) : Type :=
     { computer : vector nat arity -> itree void1 nat
-    ; computer_repr : forall arg : vector nat arity, forall rv : nat, converges_to computer arg rv <-> member (arg, rv) funGraph
-    ; partialness : isPartialFunctionGraph funGraph
+    ; computer_repr
+      : forall arg : vector nat arity, forall rv : nat, converges_to computer arg rv <-> member (arg, rv) fun_graph
+    ; partialness
+      : isPartialFunctionGraph fun_graph
     }
   .
 
