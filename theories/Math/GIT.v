b3320d1c80d832bb68ccb0f188b6ba5b354204ad
KiJeong-Lim <gijungdduk@naver.com>
2022-08-25 21:18:02 +0900

    .

--- theories/Math/GIT.v
+++ theories/Math/GIT.v
@@ -22,8 +22,8 @@ Module GIT.
 
   Local Existing Instances freeSetoidFromSetoid1.
 
-  Definition isPartialFunctionGraph {arity : nat} (fun_graph : ensemble (vector nat arity * nat)) : Prop :=
-    forall x : vector nat arity, forall y : nat, forall y' : nat, (x, y) \in fun_graph -> (x, y') \in fun_graph -> y = y'
+  Definition isPartialFunctionGraph {A : Type} {B : Type} (fun_graph : ensemble (A * B)) : Prop :=
+    forall x : A, forall y : B, forall y' : B, (x, y) \in fun_graph -> (x, y') \in fun_graph -> y = y'
   .
 
   Definition converges_to {arity : nat} (computer : vector nat arity -> itree void1 nat) (arg : vector nat arity) (rv : nat) : Prop :=
@@ -32,7 +32,7 @@ Module GIT.
 
   Record muPartial {arity : nat} (fun_graph : ensemble (vector nat arity * nat)) : Type :=
     { computer : vector nat arity -> itree void1 nat
-    ; computerRepresents : forall arg : vector nat arity, forall rv : nat, converges_to computer arg rv <-> member (arg, rv) fun_graph
+    ; computerRepresents : forall arg : vector nat arity, forall rv : nat, converges_to computer arg rv <-> (arg, rv) \in fun_graph
     ; partialness : isPartialFunctionGraph fun_graph
     }
   .
