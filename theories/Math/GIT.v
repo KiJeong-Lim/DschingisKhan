7f4e2aa8021ddec84bea4fb742b28fe77956cf39
KiJeong-Lim <gijungdduk@naver.com>
2022-08-25 17:33:04 +0900

    .

--- theories/Math/GIT.v
+++ theories/Math/GIT.v
@@ -22,4 +22,8 @@ Module GIT.
 
   Local Existing Instances freeSetoidFromSetoid1.
 
+  Definition converges_to {arity : nat} (computer : vector nat arity -> itree void1 nat) (args : vector nat arity) (rv : nat) : Prop :=
+    exists fuel : nat, burnTau_nat fuel (computer args) == Ret rv
+  .
+
 End GIT.
