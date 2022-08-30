9a0848c67e7cf58704e224455e130d456840dcf1
KiJeong-Lim <gijungdduk@naver.com>
2022-08-30 23:18:18 +0900

    add Category.v

--- theories/Data/Category.v
+++ theories/Data/Category.v
@@ -7,7 +7,17 @@ Require Import DschingisKhan.Prelude.PreludeInit.
 
 Module Categories.
 
-  Local Polymorphic Instance FunctorCat (src : isCategory) (tgt : isCategory) : isCategory :=
+  Section InstancesOfCategory.
+
+  Local Polymorphic Instance CategoryOfCategories : isCategory :=
+    { objs := isCategory
+    ; hom src tgt := src -----> tgt
+    ; compose {C} {C'} {C''} f2 f1 := fun X => f2 (f1 X)
+    ; id {C} := fun X => X
+    }
+  .
+
+  Local Polymorphic Instance CategoryOfFunctors (src : isCategory) (tgt : isCategory) : isCategory :=
     { objs := src -----> tgt
     ; hom F F' := F =====> F'
     ; compose {F} {F'} {F''} f2 f1 := fun X => compose (f2 X) (f1 X)
@@ -15,19 +25,17 @@ Module Categories.
     }
   .
 
-  Local Instance HaskEndo : isCategory := FunctorCat Hask.cat Hask.cat.
+  Local Instance HaskEndo : isCategory := CategoryOfFunctors Hask.cat Hask.cat.
+
+  End InstancesOfCategory.
+
+  Section CATEGORICAL_SUM.
 
   Polymorphic Class hasCoproduct (cat : isCategory) : Type :=
     { Sum (obj_l : cat.(objs)) (obj_r : cat.(objs)) : cat.(objs)
     ; Inl {obj_l : cat.(objs)} {obj_r : cat.(objs)} : cat.(hom) (obj_l) (Sum obj_l obj_r)
     ; Inr {obj_l : cat.(objs)} {obj_r : cat.(objs)} : cat.(hom) (obj_r) (Sum obj_l obj_r)
     ; Case {obj_l : cat.(objs)} {obj_r : cat.(objs)} {obj : cat.(objs)} (left : cat.(hom) (obj_l) obj) (right : cat.(hom) (obj_r) obj) : cat.(hom) (Sum obj_l obj_r) obj
-    }
-  .
-
-  Polymorphic Class hasInitial (cat : isCategory) : Type :=
-    { Void : cat.(objs)
-    ; ExFalso {obj : cat.(objs)} : cat.(hom) Void obj
     }
   .
 
@@ -47,12 +55,6 @@ Module Categories.
     }
   .
 
-  Local Instance Hask_hasInitial : hasInitial Hask.cat :=
-    { Void := Empty_set
-    ; ExFalso (A : Type) := @Empty_set_rect (fun _ : Empty_set => A)
-    }
-  .
-
   Local Instance functorCat_hasCoproduct : hasCoproduct HaskEndo :=
     { Sum := sum1
     ; Inl {FL : Type -> Type} {FR : Type -> Type} (X : Type) := inl1 (FL := FL) (FR := FR) (X := X)
@@ -61,10 +63,28 @@ Module Categories.
     }
   .
 
+  End CATEGORICAL_SUM.
+
+  Section INITIAL_OBJECT.
+
+  Polymorphic Class hasInitial (cat : isCategory) : Type :=
+    { Void : cat.(objs)
+    ; ExFalso {obj : cat.(objs)} : cat.(hom) Void obj
+    }
+  .
+
+  Local Instance Hask_hasInitial : hasInitial Hask.cat :=
+    { Void := Empty_set
+    ; ExFalso (A : Type) := @Empty_set_rect (fun _ : Empty_set => A)
+    }
+  .
+
   Local Instance functorCat_hasInitial : hasInitial HaskEndo :=
     { Void := void1
     ; ExFalso (F : Type -> Type) (X : Type) := @void1_rect X (fun _ : void1 X => F X)
     }
   .
+
+  End INITIAL_OBJECT.
 
 End Categories.
